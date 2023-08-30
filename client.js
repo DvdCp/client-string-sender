20830e74414e76037d6e583435b0d3be7fb3609a
DavidC <dav.capuano98@gmail.com>
2023-08-30 19:52:56 +0200

    first readme.md & adding service name as host

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
-const net = require('net');
+const net = require("net");
 
 function generateRandomString(length) {
-  const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let randomString = '';
+  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let randomString = "";
 
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * letters.length);
@@ -12,29 +12,29 @@ function generateRandomString(length) {
   return randomString;
 }
 
-const serverHost = 'localhost'; // Change this to the server's hostname or IP address
-const serverPort = 3000; // Change this to the server's port
+const serverHost = "server-string-receiver";
+const serverPort = 3000;
 
 const client = new net.Socket();
 let stringInterval;
 
 client.connect(serverPort, serverHost, () => {
-    console.log('Connected to server');
+  console.log("Connected to server");
 
-    stringInterval = setInterval(() => {
-        const length = 10; // Change this to the desired length of the string
-        const message = generateRandomString(length);;
-        client.write(message);
-        console.log(`Sent: ${message}`);
-    }, 2000);
+  stringInterval = setInterval(() => {
+    const length = 10; // Change this to the desired length of the string
+    const message = generateRandomString(length);
+    client.write(message);
+    console.log(`Sent: ${message}`);
+  }, 2000);
 });
 
-client.on('data', data => {
-    console.log(`Received: ${data}`);
+client.on("data", (data) => {
+  console.log(`Received: ${data}`);
 });
 
-client.on('close', () => {
-    console.log('Connection closed');
-    clearInterval(stringInterval);
-    client.destroy();
+client.on("close", () => {
+  console.log("Connection closed");
+  clearInterval(stringInterval);
+  client.destroy();
 });
